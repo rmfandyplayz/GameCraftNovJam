30b59f620b9361e0cb7b8a8d83b1e0138fae2800
MysticTortoise <shadowknight7788@gmail.com>
2025-11-15 14:50:12 -0700

    rats n stuff

---
+++ R A T S!/Assets/Scripts/Items/GrabbableBase.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GrabbableBase : MonoBehaviour
+{
+    public Vector3 offsetPos;
+    public float offsetRotation;
+
+
+    public virtual void Use(Player player)
+    {
+        
+    }
+    
+
+}
