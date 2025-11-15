b2b52f8d3b8e7cf8c7fda5b1c0a365ee89b4baa6
rmfandyplayz <58617083+rmfandyplayz@users.noreply.github.com>
2025-11-15 00:17:14 -0700

    changes for today

--- R A T S!/Assets/Scripts/UI/MainMenu.cs
+++ R A T S!/Assets/Scripts/UI/MainMenu.cs
@@ -1,27 +1,8 @@
 using UnityEngine;
 using DG.Tweening;
 
-/// <summary
-
 public class MainMenu : MonoBehaviour
 {
 
-    
-
-    // animations
-    public void PlayButtonHoverAnim()
-    {
-
-    }
-
-
-    public void GenericButtonHoverAnim()
-    {
-
-    }
-
-    public void GenericButtonUnhoverAnim()
-    {
-
-    }
+   
 }
---
+++ R A T S!/Assets/Scripts/UI/PlayButton.cs
@@ -0,0 +1,128 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class PlayButton : GenericMenuButton
+{
+    private bool isInteractable = true;
+
+    [SerializeField] Button button;
+
+    private Coroutine pointerEnterCoroutine;
+    private Coroutine pointerExitCoroutine;
+
+    [SerializeField] const float scaleUpSize = 1.15f;
+    [SerializeField] const float rippleEffectDistance = 25f;
+    [SerializeField] const float animationDuration = 0.35f;
+
+    GenericMenuButton[] menuButtons; 
+
+    void Start()
+    {
+        SetOriginalPosition();
+        SetTargetPos(rippleEffectDistance);
+        menuButtons = FindObjectsByType<GenericMenuButton>(FindObjectsSortMode.None);
+    }
+
+    public override void OnClick()
+    {
+        if (!isInteractable)
+            return;
+
+        foreach(MonoBehaviour type in FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None))
+        {
+            if(type is GenericMenuButton genericButton)
+            {
+                genericButton.DisableButtonInteractions();
+            }
+        }
+    }
+
+    public override void OnPointerEnter()
+    {
+        if (!isInteractable)
+            return;
+
+        foreach (Button b in FindObjectsByType<Button>(FindObjectsSortMode.None))
+        {
+            DOTween.Kill(b);
+        }
+
+        button.image.rectTransform.DOScale(new Vector2(scaleUpSize, scaleUpSize), animationDuration).SetEase(Ease.OutBack);
+
+        foreach (Button b in FindObjectsByType<Button>(FindObjectsSortMode.None))
+        {
+            if (b.gameObject != this.gameObject)
+            {
+                if (b.image.rectTransform.anchoredPosition.y < button.image.rectTransform.anchoredPosition.y)
+                    b.image.rectTransform.DOAnchorPosY(b.image.rectTransform.anchoredPosition.y - rippleEffectDistance, animationDuration).SetEase(Ease.OutBack);
+                else
+                    b.image.rectTransform.DOAnchorPosY(b.image.rectTransform.anchoredPosition.y + rippleEffectDistance, animationDuration).SetEase(Ease.OutBack);
+            }
+        }
+    }
+
+    //public override void OnPointerEnter()
+    //{
+    //    foreach (Button b in FindObjectsByType<Button>(FindObjectsSortMode.None))
+    //    {
+    //        DOTween.Kill(b);
+    //    }
+
+    //    button.image.rectTransform.DOScale(new Vector2(scaleUpSize, scaleUpSize), animationDuration).SetEase(Ease.OutBack);
+
+    //    foreach (var genBtn in menuButtons)
+    //    {
+    //        if (genBtn.gameObject == this.gameObject)
+    //            continue;
+
+    //        Button btn = genBtn.GetComponent<Button>();
+    //        if (btn == null)
+    //            continue;
+
+    //        btn.image.rectTransform
+    //            .DOAnchorPosY(genBtn.GetTargetPos().y, animationDuration)
+    //            .SetEase(Ease.OutBack);
+    //    }
+    //}
+
+    public override void OnPointerExit()
+    {
+        foreach (Button b in FindObjectsByType<Button>(FindObjectsSortMode.None))
+        {
+            DOTween.Kill(b);
+        }
+
+        button.image.rectTransform
+            .DOScale(Vector2.one, animationDuration)
+            .SetEase(Ease.OutBack);
+
+        GenericMenuButton[] menuButtons = FindObjectsByType<GenericMenuButton>(FindObjectsSortMode.None);
+
+        foreach (var genBtn in menuButtons)
+        {
+            if (genBtn.gameObject == this.gameObject)
+                continue;
+
+            Button btn = genBtn.GetComponent<Button>();
+            if (btn == null)
+                continue;
+
+            btn.image.rectTransform
+                .DOAnchorPosY(genBtn.GetOriginalPos().y, animationDuration)
+                .SetEase(Ease.OutBack);
+        }
+    }
+
+
+    public override void DisableButtonInteractions()
+    {
+        isInteractable = false;
+        button.interactable = false;
+    }
+    public override void EnableButtonInteractions()
+    {
+        isInteractable = true;
+        button.interactable = true;
+    }
+}
