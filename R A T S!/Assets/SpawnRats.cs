406d9ba4e7e9b1e9e2c0452d6ce5611e0d1c122a
MysticTortoise <shadowknight7788@gmail.com>
2025-11-15 15:51:06 -0700

    rat spawning shit idk

--- R A T S!/Assets/SpawnRats.cs
+++ R A T S!/Assets/SpawnRats.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using NUnit.Framework;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnRats : MonoBehaviour
 {
@@ -10,6 +12,11 @@ public class SpawnRats : MonoBehaviour
     public List<GameObject> ratPrefabs;
     private float timer;
     public int spawnThreshold;
+
+    [SerializeField] private float spawnDist;
+
+    private Player player;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,7 +26,8 @@ public class SpawnRats : MonoBehaviour
         {
             spawnPoints.Add(point.transform);
         }
-        Debug.Log(spawnPoints.Count);
+
+        player = FindAnyObjectByType<Player>();
     }
 
     // Update is called once per frame
@@ -35,7 +43,24 @@ public class SpawnRats : MonoBehaviour
 
     void spawnRat()
     {
-        int spawnIndex = Random.Range(0, spawnPoints.Count);
+        int spawnIndex = 0;
+        int tries = 0;
+        while (tries < 10)
+        {
+            tries++;
+            spawnIndex = Random.Range(0, spawnPoints.Count);
+            float dist = (spawnPoints[spawnIndex].position - player.transform.position).magnitude;
+            if (dist > spawnDist)
+            {
+                break;
+            }
+        }
+
+        if (tries >= 9)
+        {
+            Debug.LogWarning("Failed to spawn rat!");
+            return;
+        }
         int ratIndex = Random.Range(0, ratPrefabs.Count);
         GameObject rat = Instantiate(ratPrefabs[ratIndex], spawnPoints[spawnIndex].position, Quaternion.identity);
         rat.name = "rat";
@@ -48,6 +73,15 @@ public class SpawnRats : MonoBehaviour
         foreach(GameObject item in GameObject.FindGameObjectsWithTag("Spawn Point"))
         {
             Gizmos.DrawSphere(item.transform.position, .5f);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        foreach(GameObject item in GameObject.FindGameObjectsWithTag("Spawn Point"))
+        {
+            Gizmos.DrawWireSphere(item.transform.position, spawnDist);
         }
     }
 }
