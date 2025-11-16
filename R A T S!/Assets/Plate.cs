2bfe2275acd65b83e905e95adef50f456fa7a585
MysticTortoise <shadowknight7788@gmail.com>
2025-11-15 17:14:10 -0700

    work on objective stuff

--- R A T S!/Assets/Plate.cs
+++ R A T S!/Assets/Plate.cs
@@ -1,16 +1,45 @@
+using System;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Plate : GrabbableBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Vector2 checkForThings;
+    [SerializeField] private LayerMask layerMask;
+
+    [CanBeNull] private FoodItem currentItem;
+
+
+    public override void Use(Player player)
     {
-        
+        foreach (Collider2D collider in  Physics2D.OverlapBoxAll(transform.position, checkForThings, 0, layerMask))
+        {
+            Debug.Log(collider.gameObject.name);
+            if (!collider.gameObject.CompareTag("FoodObjective")) continue;
+            Debug.Log("IS AN OBJECTIVE THINGY!");
+            
+            if (collider.GetComponent<FoodItem>() is { } item)
+            {
+                currentItem ??= item;
+            } else if (collider.GetComponent<Oven>() is { } oven)
+            {
+                if (currentItem is null) return;
+                oven.StoreItem(currentItem);
+                currentItem = null;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!currentItem) return;
+
+        currentItem.transform.position = transform.position;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.position, checkForThings);
     }
 }
