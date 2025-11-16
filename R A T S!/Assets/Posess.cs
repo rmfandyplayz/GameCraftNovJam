1b0753622ef84bc36b2d82b8d287cfb4e1df2c1d
ocfrey <ocfrey270@gmail.com>
2025-11-16 11:30:10 -0700

    some audio files are in

--- R A T S!/Assets/Posess.cs
+++ R A T S!/Assets/Posess.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class Posess : MonoBehaviour
 {
+    private Animator playerAnim;
+    private Transform player;
+    private float posessTimer;
+    public float posessTime;
+    private Player playerScript;
+    private bool possessionOnCooldown = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerAnim = FindAnyObjectByType<Player>().GetComponent<Animator>();
+        player = FindAnyObjectByType<Player>().transform;
+        playerScript = FindAnyObjectByType<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        posessTimer += Time.deltaTime;
+        if (posessTimer >= posessTime)
+        {
+            playerAnim.SetBool("Possessed", false);
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.layer);
+        if (collision.gameObject.layer.Equals("Player") && !possessionOnCooldown)
+        {
+            possessionOnCooldown = true;
+            Posession();
+        }
+    }
+
+    void Posession()
+    {
+        playerAnim.SetBool("Possessed", true);
     }
 }
