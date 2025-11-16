cb9b701f7db7ddd0a4da91e6fcc22623efe4c82c
MysticTortoise <shadowknight7788@gmail.com>
2025-11-15 20:03:57 -0700

    plate stuff

--- R A T S!/Assets/Scripts/Plate.cs
+++ R A T S!/Assets/Scripts/Plate.cs
@@ -1,16 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Plate : GrabbableBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Vector2 checkForThings;
+    [SerializeField] private LayerMask layerMask;
+
+    private Stack<FoodItem> foodItems = new();
+
+    [SerializeField] private Vector3 itemOffset;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Use(Player player)
     {
-        
+        foreach (Collider2D collider in  Physics2D.OverlapBoxAll(transform.position, checkForThings, 0, layerMask))
+        {
+            if (!collider.gameObject.CompareTag("FoodObjective")) continue;
+            
+            if (collider.GetComponent<FoodItem>() is { } item)
+            {
+                foodItems.Push(item);
+                collider.enabled = false;
+            } else if (collider.GetComponent<Oven>() is { } oven)
+            {
+                if (foodItems.Count == 0) return;
+                oven.StoreItem(foodItems.Pop());
+            }
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < foodItems.Count; i++)
+        {
+            FoodItem item = foodItems.ToArray()[i];
+            if (!item)
+            {
+                continue;
+            }
+            item.transform.position = transform.position + itemOffset * i;
+            item.spriteRenderer.sortingOrder = spriteRenderer.sortingOrder + (i+1);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.position, checkForThings);
     }
 }
