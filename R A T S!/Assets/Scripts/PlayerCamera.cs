8bb2e70cb607d0fb759284c2b343483714fa2c22
Jackson Sipes <jacksondsipes@gmail.com>
2025-11-15 23:48:28 -0700

    shake void on camera

--- R A T S!/Assets/Scripts/PlayerCamera.cs
+++ R A T S!/Assets/Scripts/PlayerCamera.cs
@@ -7,17 +7,49 @@ public class PlayerCamera : MonoBehaviour
 
     [SerializeField] private float speed;
 
-    private static Vector3 offset = new( 0, 0, -10);
+    private static Vector3 offset = new(0, 0, -10);
+
+    private float shakeDuration = 0f;
+    private float shakeMagnitude = 0.05f;   
+    private float shakeSpeed = 40f;         
+    private Vector3 originalPos;
 
     private void Start()
     {
         player = FindAnyObjectByType<Player>();
+        originalPos = transform.localPosition;
     }
-
 
     private void FixedUpdate()
     {
-        transform.position =
-            MysticUtil.DampVector(transform.position, player.transform.position + offset, speed, Time.deltaTime);
+        // Follow player
+        Vector3 targetPos = MysticUtil.DampVector(
+            transform.position,
+            player.transform.position + offset,
+            speed,
+            Time.deltaTime
+        );
+
+        transform.position = targetPos + GetShakeOffset();
+    }
+
+    public void ShakeCamera(float duration, float magnitude = 0.05f)
+    {
+        shakeDuration = duration;
+        shakeMagnitude = magnitude;
+    }
+
+    private Vector3 GetShakeOffset()
+    {
+        if (shakeDuration <= 0f)
+            return Vector3.zero;
+
+        shakeDuration -= Time.deltaTime;
+
+        // quick oscillating shake
+        float x = Mathf.Sin(Time.time * shakeSpeed) * shakeMagnitude;
+        float y = Mathf.Cos(Time.time * shakeSpeed) * shakeMagnitude;
+
+        return new Vector3(x, y, 0f);
     }
 }
